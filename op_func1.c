1a8e9468a3f58e18c3331144d572b9bf07c37567
Nels <nelsfichera@gmail.com>
2021-05-12 07:24:58 -0400

    Add pall/swap/nop

--- op_func1.c
+++ op_func1.c
@@ -1,6 +1,28 @@
 #include "monty.h"
 
 /**
+* pall - prints every value in the stack
+* @stack: head of the doubly linked list
+* @line_number: line count
+* Return: void
+*/
+void pall(stack_t **stack, unsigned int line_number)
+{ 
+	/*needs error handling*/
+	int iter;
+	stack_t *node;
+	(void)line_number;
+
+	node = *stack;
+
+	for (x = 0; node; x++)
+	{
+		fprintf(stdout, "%d\n", node->n);
+		node = node->next;
+	}
+}
+
+/**
  * pint - prints the value at the top of the stack
  * @stack: head of the doubly linked list
  * @line_number: line count
@@ -17,3 +39,45 @@ void pint(stack_t **stack, unsigned int line_number)
 	}
 	printf("%d\n", h->n);
 }
+
+/**
+* swap - swaps two top elements
+* @stack: head of the doubly linked list
+* @line_number: line count
+* Return: void
+*/
+void swap(stack_t **stack, unsigned int line_number)
+{
+	stack_t *h = *stack;
+	int check;
+	stack_t *index0, *index1;
+
+	check = dlistint_len(h);
+	
+	/*needs error handling 
+	 * if (check < 2)*/
+	
+	index0 = head;
+	index1 = head->next;
+	index0->next = index1->next;
+	index0->prev = index1;
+	
+	if (index1->next)
+		index1->next->prev = index0;
+	
+	index1->next = index0;
+	index1->prev = NULL;
+	(*stack) = index1;
+}
+
+/**
+* nop - does nothing
+* @stack: head of the doubly linked list
+* @line_number: line count
+* Return: void
+*/
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}	
