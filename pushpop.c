4ea6bc4f4feb4c6b4ae9ce335932baccc272c4cd
Nels <nelsfichera@gmail.com>
2021-05-12 08:02:05 -0400

    Add error handling

--- pushpop.c
+++ pushpop.c
@@ -10,8 +10,11 @@ void pop(stack_t **stack, unsigned int line_number)
 	stack_t *popper = *stack;
 	(void) line_number;
 
-	/*if (*stack == NULL)
-	  * handle error*/
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n" info.l_number);
+		free_info();
+	}
 	popper = popper->next;
 	free(*stack);
 	*stack = popper;
@@ -31,8 +34,17 @@ void push(stack_t **stack, unsigned int line_number)
 	void(line_number);
 
 	n_node = malloc(sizeof(stack_t));
-	/*if (n_node == NULL 
-	  * handle error*/
+	if (n_node == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		free_info();
+	}
+	if (isdigit(info.arg[1]) > 0)
+	{
+		fprintf(stderr, "L%d: usage push integer\n", info.l_number);
+		free_info();
+	}
+
 	n_node->n = atoi(info.arg[1]);
 	if (info.type == LIFO)
 		add_node_lifo(stack, n_node);
