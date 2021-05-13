29dfafbde662f9f9d9029c18a33815d57e2e97ae
Carson Stearn <cstearn@gmail.com>
2021-05-13 20:13:46 +0000

    fix stuff

--- monty.h
+++ monty.h
@@ -55,6 +55,8 @@ void push(stack_t **head, int n);
 void pop(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
+void pchar(stack_t **stack, unsigned int line_number);
+void pstr(stack_t **stack, unsigned int line_number);
 /* calculator functions */
 void _add(stack_t **stack, unsigned int line_number);
 void _sub(stack_t **stack, unsigned int line_number);
