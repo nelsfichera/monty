67ad30fb5dae31df0fa48951264b50912b281b53
Nels <nelsfichera@gmail.com>
2021-05-16 10:45:10 -0400

    Add free

--- pushpop.c
+++ pushpop.c
@@ -13,7 +13,7 @@ void pop(stack_t **stack, unsigned int line_number)
 	if (*stack == NULL)
 	{
 		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
-		/*free_list(&stack);*/
+		free_all(NULL, NULL);
 	}
 	popper = popper->next;
 	free(*stack);
