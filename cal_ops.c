776fd767d0382fdde44d03953e1bea710912690a
Carson Stearn <cstearn@gmail.com>
2021-05-19 13:18:42 +0000

    new line

--- cal_ops.c
+++ cal_ops.c
@@ -63,7 +63,7 @@ void _div(stack_t **stack, unsigned int line_number)
 	if ((*stack)->n == 0)
 	{
 		/* free stack? */
-		fprintf(stderr, "L%d: division by zero", line_number);
+		fprintf(stderr, "L%d: division by zero\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	(*stack)->next->n /= (*stack)->n;
